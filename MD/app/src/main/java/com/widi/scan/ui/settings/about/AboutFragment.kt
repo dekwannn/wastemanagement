525eb9254435007a16fa290ec45cd94c28203306
dekwannn <kdwidiadnyana51@gmail.com>
2024-06-18 09:15:42 +0800

    about page

---
+++ MD/app/src/main/java/com/widi/scan/ui/settings/about/AboutFragment.kt
@@ -0,0 +1,32 @@
+package com.widi.scan.ui.settings.about
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.widi.scan.databinding.FragmentAboutBinding
+import com.widi.scan.ui.utils.safeNavigate
+
+class AboutFragment : Fragment() {
+
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBack.setOnClickListener {
+            findNavController().safeNavigate(AboutFragmentDirections.actionAboutFragmentToSettingsFragment())
+        }
+    }
+}
